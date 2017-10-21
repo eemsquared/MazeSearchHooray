14d5530899fe1851a0012b88b684f2a18249d90c
rmjfamat <rjfamat@up.edu.ph>
2017-10-21 20:17:45 +0700

    added maze to array

---
+++ src/MazeSearchHooray/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		SearchMaze file = new SearchMaze();
+		char[][] arrayFile = file.getArrayFile("sample.txt");
+		int row = file.getRowSize();
+		int col = file.getColSize();
+
+	}
+
+}
